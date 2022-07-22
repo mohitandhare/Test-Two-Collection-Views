b6d1d166d91d720528ed80c608090e22581b0252
Mohit Andhare <mohitandhare@skromanglobal.com>
2022-07-22 10:21:23 +0530

    Initial Commit

---
+++ Test Two Collection Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test Two Collection Views
+//
+//  Created by Developer Skromanglobal on 22/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
